516c625ab3a975e51f11eeca1a7d1178cc4c8d9c
Mohamed Tobal <mohamedtobal369@gmail.com>
2023-08-04 16:28:48 +0300

    Up

--- js/script.js
+++ js/script.js
@@ -1,39 +1,34 @@
-// $$$ Start Script $$$
+document.addEventListener("DOMContentLoaded", function () {
+    const bar = document.getElementById("bar");
+    const closed = document.getElementById("x-mark");
+    const nav = document.querySelector(".navbar");
 
-// --- Start For Header ---
-let ulElements = Array.from(document.querySelectorAll("header .navbar li a"))
-ulElements.forEach((e) => {
-    console.log(e)
-    e.onclick = function () {
-        ulElements.forEach((e) => {
-            e.classList.remove("active")
-        })
-        e.classList.add("active")
+    if (bar) {
+        bar.addEventListener("click", function() {
+            nav.classList.add('active');
+            bar.classList.add("active");
+            nav.classList.remove('close');
+        });
     }
-})
 
-// If Onclick On The Bar Menu
-let bar = document.getElementById("bar")
-let nav = document.querySelector(".navbar")
-let closed = document.getElementById("x-mark")
+    if (closed) {
+        closed.addEventListener("click", function() {
+            nav.classList.add('close');
+            nav.classList.remove('active');
+            bar.classList.remove("active");
+        });
+    }
 
-if (bar) {
-    bar.addEventListener("click", function() {
-        nav.classList.add('active')
-        bar.classList.add("active")
-        nav.classList.remove('close')
-    })
-}
-if (closed) {
-    closed.addEventListener("click", function() {
-        nav.classList.add('close')
-        nav.classList.remove('active')
-        bar.classList.remove("active")
-
-    })
-}
-
-
-// --- End For Header ---
-
-// $$$ End Script $$$
+    // Add a click event listener to the document
+    document.addEventListener("click", function (event) {
+        // Check if the clicked element is the navbar or the toggle button
+        const isNavbarClicked = nav.contains(event.target) || event.target === bar;
+    
+        // If the click is outside the navbar and it's not the toggle button, close the navbar
+        if (!isNavbarClicked) {
+            nav.classList.remove('active');
+            nav.classList.add('close');
+            bar.classList.remove("active");
+        }
+    });
+});
